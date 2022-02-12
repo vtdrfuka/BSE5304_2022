11a44e9b9704331bd778981aad568b20f6d40888
vtdrfuka <80080115+vtdrfuka@users.noreply.github.com>
2022-02-11 21:57:16 -0700

    Create Lab04.R

---
+++ Lab04.R
@@ -0,0 +1,135 @@
+options(repos ="http://cran.us.r-project.org")  # required to get latest libs
+# Installing the packages we will play with today
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(elevatr,soilDB,rgdal,raster)
+# From previous weeks
+pacman::p_load(EcoHydRology,rnoaa,curl,httr)
+rm(list=objects())
+setwd("~")
+dir.create("~/Lab04")
+setwd("~/Lab04/")
+# 3 Functions to calculate SWE and excess when soil is drying, 
+#   wetting, and wetting above capacity
+#
+browseURL("https://github.com/vtdrfuka/BSE5304_2022/tree/main/functions")
+browseURL("https://github.com/vtdrfuka/BSE5304_2022/blob/main/functions/TMWBmodel.R")
+browseURL("https://raw.githubusercontent.com/vtdrfuka/BSE5304_2022/main/functions/NSE.R")
+source("https://raw.githubusercontent.com/vtdrfuka/BSE5304_2022/main/functions/TMWBmodel.R")
+
+# Download a soils dataset for your basin based on the WebSoilSurvey method 
+# and replace this url with your own
+url="https://websoilsurvey.sc.egov.usda.gov/DSD/Download/AOI/bky5q4i2wkdjmvm3t0d1gniq/wss_aoi_2022-02-11_09-31-24.zip"
+download.file(url,"mysoil.zip")
+unzip("mysoil.zip")
+# https://cran.r-project.org/web/packages/elevatr/elevatr.pdf
+# https://cran.r-project.org/web/packages/raster/raster.pdf
+# https://cran.r-project.org/web/packages/soilDB/soilDB.pdf
+# https://cran.r-project.org/web/packages/rgdal/rgdal.pdf
+# use the function to get data from USGS 0205551460 
+#LICK RUN ABOVE PATTON AVENUE AT ROANOKE, VA
+myflowgage_id="0205551460"
+myflowgage=get_usgs_gage(myflowgage_id,begin_date = "2015-01-01",end_date = "2022-03-01")
+# Note that flow returned is in m3/day, but we want mm/day for the basin
+myflowgage$flowdata$Qmm = myflowgage$flowdata$flow/myflowgage$area/10^3
+# the soil data using the soilDB package
+# What is this returning? Why do we care?
+# This needs to be completed based on your download
+mysoil=readOGR("wss_aoi_2022-02-11_09-31-24/spatial/soilmu_a_aoi.shp")    
+# Explore the mysoil dataset which is returned
+mybbox=c(mysoil@bbox)
+# First associate mukey with cokey from component
+mysoil$mukey=mysoil$MUKEY  # or rename the column
+mukey_statement = format_SQL_in_statement(unique(mysoil$mukey))
+print(mukey_statement)
+q_mu2co = paste("SELECT mukey,cokey FROM component WHERE mukey IN ", mukey_statement, sep="")
+print(q_mu2co)
+mu2co = SDA_query(q_mu2co)
+# Second associate cokey with ksat_r,awc_r,hzdepb_r from chorizon
+cokey_statement = format_SQL_in_statement(unique(mu2co$cokey))
+q_co2ch = paste("SELECT cokey,ksat_r,awc_r,hzdepb_r  FROM chorizon WHERE cokey IN ", cokey_statement, sep="")
+print(q_co2ch)
+co2ch = SDA_query(q_co2ch)
+# Last, bring them back together, and aggregate based on max values
+# of ksat_r,awc_r, and hzdepb_r
+mu2ch=merge(mu2co,co2ch)
+summary(mu2ch)
+mu2chmax=aggregate(mu2ch,list(mu2ch$mukey),max)
+
+proj4_ll = "+proj=longlat"
+proj4string(mysoil) = proj4_ll
+mydem=get_elev_raster(locations=mysoil, 
+                        z = 11, prj =proj4string(mysoil) ,
+                        src ="aws",clip="bbox",expand = 0.001)
+
+summary(terrain(mydem, opt='slope',unit = "degrees"))
+# What is this 'slope'? Use the man page for the terrain() function to answer
+plot(terrain(mydem, opt='TPI',unit = "degrees"))
+# What is this 'TPI'? 
+summary(terrain(mydem, opt='TRI',unit = "degrees"))
+plot(terrain(mydem, opt='TRI',unit = "degrees"))
+# What is this 'TRI'? 
+
+stns=meteo_distance(
+  station_data=ghcnd_stations(),
+  lat=myflowgage$declat,
+  long=myflowgage$declon,
+  units = "deg",
+  radius = 30,
+  limit = NULL
+)
+# We are looking for stations with elements that have PRCP, TMAX and TMIN 
+# and current data (i.e. Year 2021). 
+WXStn=stns[stns$element=="TMAX"&stns$last_year>=2021,]$id[1]
+WXData=meteo_pull_monitors(
+  monitors=WXStn,
+  keep_flags = FALSE,
+  date_min = "2016-01-01",
+  date_max = NULL,
+  var = c("TMAX","TMIN","PRCP") 
+)
+summary(WXData)  #
+
+# Create an aligned modeldata data frame to build our model in
+modeldata=merge(WXData,myflowgage$flowdata,by.x="date",by.y="mdate")
+summary(modeldata)  #
+modeldata$MaxTemp=modeldata$tmax/10 # Converting to C
+modeldata$MinTemp=modeldata$tmin/10 # Converting to C
+modeldata$P=modeldata$prcp/10 # Converting to mm
+# View(modeldata)  
+# Compare your precipitation to the flow out of your basin
+modeldata$P[is.na(modeldata$P)]=0
+modeldata$MinTemp[is.na(modeldata$MinTemp)]=0
+modeldata$MaxTemp[is.na(modeldata$MaxTemp)]=
+  modeldata$MinTemp[is.na(modeldata$MaxTemp)] +1
+modeldata$MaxTemp[modeldata$MaxTemp<=modeldata$MinTemp]=
+  modeldata$MinTemp[modeldata$MaxTemp<=modeldata$MinTemp]+1
+modeldata$AvgTemp=(modeldata$MaxTemp+modeldata$MaxTemp)/2.0
+
+summary(modeldata)
+modeldata[is.na(modeldata)]=0 # A Quick BUT sloppy removal of NAs
+TMWB=modeldata
+# Last weeks homework example
+source("https://raw.githubusercontent.com/vtdrfuka/BSE5304_2022/main/functions/TMWBmodel.R")
+# Calibrating the parameters one at a time
+for (fcres in seq(.1,.5,.1)){
+  TMWBnew=TMWBmodel(TMWB=TMWB,fcres=fcres)
+  print(paste(fcres,NSE(TMWBnew$Qmm,TMWBnew$Qpred)))
+}
+# fcres=.3 is the highest NSE
+for (SFTmp in seq(-5,20)){
+  TMWBnew=TMWBmodel(TMWB=TMWB,fcres=.3,SFTmp = SFTmp)
+  print(paste(SFTmp,NSE(TMWBnew$Qmm,TMWBnew$Qpred)))
+}
+for(AWCval in seq(50,350,50)){
+  TMWBnew=TMWBmodel(TMWB=TMWB,fcres=.3,SFTmp = 11,Tlag = .5,AWCval = AWCval)
+  print(paste(AWCval,NSE(TMWBnew$Qmm,TMWBnew$Qpred)))
+}
+# Best result for "LICK RUN ABOVE PATTON AVENUE AT ROANOKE, VA" NSE = .34 . 
+TMWBnew=TMWBmodel(TMWB=TMWB,fcres=.3,SFTmp = 11,Tlag = .5,AWCval = 100)
+print(paste(AWCval,NSE(TMWBnew$Qmm,TMWBnew$Qpred)))
+
+# For a simple spatial model, use TMWB, to initialize 3 
+# slope components for Top, Mid, and Bottom of the hillside. 
+TopSlope=TMWB
+MidSlope=TMWB
+BotSlope=TMWB
